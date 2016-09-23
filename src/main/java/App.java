fc2cf8acc7b20eb1aa0aed5d74c68a13f707c741
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-09-23 15:54:52 -0700

    add client appointment date to client list

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -31,5 +31,28 @@ public class App {
       response.redirect("/");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/:name/clients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.findName(request.params(":name"));
+      model.put("title", stylist.getName() + "'s Clients");
+      model.put("clients", Client.all());
+      model.put("header", header);
+      model.put("stylist", stylist);
+      model.put("template", "templates/stylist-details.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/:name/client/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Stylist stylist = Stylist.findName(request.params(":name"));
+      String name = request.queryParams("name");
+      String notes = request.queryParams("notes");
+      String appointment_date = request.queryParams("appointment_date");
+      Client newClient = new Client(name, notes, appointment_date, stylist.getId());
+      newClient.save();
+      response.redirect("/");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
